e36336549faa51adc6828292b8f9669617f590e0
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2023-03-28 20:05:04 -0300

    feat: finish text animation and button

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,14 +1,28 @@
-var texto = "Hello, welcome to my website! sdsdsdsdsdsdsdsdsds";
-
-function digitar(i, texto, elementoTexto, elementoCursor) {
-	if (i < texto.length) {
-		elementoTexto.innerHTML += texto.charAt(i);
-		elementoCursor.style.left = elementoTexto.offsetWidth + "px";
+function writeText(text, element, interval) {
+	let i = 0;
+	const timer = setInterval(function() {
+	  if (i < text.length) {
+		const novotext = element.innerHTML + text.charAt(i);
+		element.innerHTML = novotext;
 		i++;
-		setTimeout(function() { digitar(i, texto, elementoTexto, elementoCursor); }, 50);
-	}
-}
+	  } else {
+		clearInterval(timer);
+		const cursor = document.createElement("span");
+		cursor.className = "cursor";
+		element.appendChild(cursor);
+		setInterval(function() {
+		  cursor.style.display = cursor.style.display === "none" ? "" : "none";
+		}, 500);
+	  }
+	}, interval);
+  }
 
-var elementoTexto = document.getElementById("texto");
-var elementoCursor = document.getElementById("cursor");
-digitar(0, texto, elementoTexto, elementoCursor);
+  
+  const text = "Hello, welcome to my portfolio! Click 'Next' to continue...";
+
+
+  const element = document.getElementById("text-content");
+  const interval = 50;
+  writeText(text, element, interval);
+
+  
