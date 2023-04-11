feeebf2b3caaa4c03eef2b61541b0d364085766f
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2023-04-11 16:56:05 -0300

    feat: create a page of soft skills

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,29 +1,10 @@
-function writeText(text, element, interval) {
-	let i = 0;
-	const timer = setInterval(function() {
-	  if (i < text.length) {
-		const novotext = element.innerHTML + text.charAt(i);
-		element.innerHTML = novotext;
-		i++;
-	  } else {
-		clearInterval(timer);
-		const cursor = document.createElement("span");
-		cursor.className = "cursor";
-		element.appendChild(cursor);
-		setInterval(function() {
-		  cursor.style.display = cursor.style.display === "none" ? "" : "none";
-		}, 500);
-	  }
-	}, interval);
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
   }
 
-  
-  const text = "Hello, welcome to my portfolio! Click 'Next' to continue...";
-
-
-  const element = document.getElementById("text-content");
-  const interval = 50;
-  writeText(text, element, interval);
-
-
-  
+const titulo = document.getElementById("text-content");
+typeWriter(titulo);
--- pages/introduction/script.js
+++ pages/introduction/script.js
@@ -1,28 +1,10 @@
-function writeText(text, element, interval) {
-	let i = 0;
-	const timer = setInterval(function() {
-	  if (i < text.length) {
-		const novotext = element.innerHTML + text.charAt(i);
-		element.innerHTML = novotext;
-		i++;
-	  } else {
-		clearInterval(timer);
-		const cursor = document.createElement("span");
-		cursor.className = "cursor";
-		element.appendChild(cursor);
-		setInterval(function() {
-		  cursor.style.display = cursor.style.display === "none" ? "" : "none";
-		}, 500);
-	  }
-	}, interval);
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 50 * i);
+    });
   }
 
-  
-  const text = "My name is Gabriel Yuri. I am a Computer Science student at the Federal University of Campina Grande (UFCG) and Member of the Jr CodeX company, working in the business sector and as a Front End and Back End developer.";
-
-
-  const element = document.getElementById("text-content");
-  const interval = 50;
-  writeText(text, element, interval);
-
-  
+const titulo = document.getElementById("text-content");
+typeWriter(titulo);
