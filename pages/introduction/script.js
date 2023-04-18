b24778efcb980fc64cdea543dcd457ca5c0a96b4
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2023-04-18 13:08:51 -0300

    new features

--- pages/introduction/script.js
+++ pages/introduction/script.js
@@ -2,9 +2,14 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 50 * i);
+      setTimeout(() => elemento.innerHTML += letra, 45 * i);
     });
   }
 
-const titulo = document.getElementById("text-content");
-typeWriter(titulo);
+
+const title = document.getElementById("title")
+typeWriter(title)
+
+const text = document.getElementById("text-content");
+typeWriter(text);
+
